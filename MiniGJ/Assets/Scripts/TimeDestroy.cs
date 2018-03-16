ec59b7e12bce3ec6f4d6b03d6cb0cfb0baf15aea
Unknown <jimmykiet17@yahoo.ca>
2018-03-16 15:31:29 -0400

    Animation

---
+++ MiniGJ/Assets/Scripts/TimeDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDestroy : MonoBehaviour {
+    public float interval;
+
+    void Update()
+    {
+        if (interval > 0)
+        {
+            interval -= Time.deltaTime;
+        }
+        else
+        {
+            enabled = false;
+
+            Destroy(gameObject);
+        }
+
+    }
+}
