ced49daa8ba3865877fb89c0e002f603bd01fac0
Antoine Chérier <a.cherier@student.isartdigital.com>
2018-03-16 11:43:25 -0400

    Deplacement perso

---
+++ MiniGJ/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector2.left * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(Vector3.forward * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(Vector3.back * 4f * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0, 0, 0);
+		}
+
+
+	}
+
+}
